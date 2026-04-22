fef16077d03369481383b5c2dab856db009876e8
tranduc567 <tranminhduc20052005@gmail.com>
2026-04-22 22:13:40 +0700

    feat: Add interaction nonlinear-basis

--- code/Part1_Regression/pipeline.py
+++ code/Part1_Regression/pipeline.py
@@ -4,8 +4,12 @@ from sklearn.compose import TransformedTargetRegressor
 from sklearn.pipeline import Pipeline
 
 
-def get_pipeline(predictor, features=None):
+def get_pipeline(predictor, features=None,use_interaction=False):
     steps = []
+    if use_interaction:
+        from nonlinear_basis.interaction import InteractionBasis
+        steps.append(("interaction", InteractionBasis()))
+
     steps.append(("scaler", StandardScaler()))
     if features is not None:
         steps.append(("features", features))
