92760a42fb8b2562f15ecebd7c985b13423c4e9f
tranduc567 <tranminhduc20052005@gmail.com>
2026-04-22 16:58:53 +0700

    feat: add validation curve for model evaluation in notebook

--- code/Part1_Regression/nonlinear_basis/polynomial.py
+++ code/Part1_Regression/nonlinear_basis/polynomial.py
@@ -1,19 +1,18 @@
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 
-# POLYNOMIAL BASIS
-class PolynomialBasis:
+class PolynomialBasis(BaseEstimator, TransformerMixin):
     def __init__(self, degree=5):
         self.degree = degree
 
-    def fit(self, X, y=None, **params):
+    def fit(self, X, y=None):
         return self
 
     def transform(self, X):
+        X = np.asarray(X)
         features = [X]
 
         for d in range(2, self.degree + 1):
             features.append(X ** d)
 
         return np.concatenate(features, axis=1)
-
